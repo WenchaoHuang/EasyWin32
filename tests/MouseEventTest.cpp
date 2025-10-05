684b4ee5697a616e1a984d87a9905c0845bfe421
Wenchao Huang <physhuangwenchao@gmail.com>
2025-10-05 14:26:52 +0800

    Add mouse and keyboard event tests
    Signed-off-by: Wenchao Huang <physhuangwenchao@gmail.com>

---
+++ tests/MouseEventTest.cpp
@@ -0,0 +1,89 @@
+/**
+ *	Copyright (c) 2025 Wenchao Huang <physhuangwenchao@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+#include <easy_win32.h>
+
+/*********************************************************************************
+******************************    MouseEventTest    ******************************
+*********************************************************************************/
+
+/**
+ *	@brief		Simple mouse event testing routine.
+ *	@details	This function listens for mouse movement and click events.
+ *	@note		Pressing the `Escape` key will end the test.
+ */
+void MouseEventTest(EzWindow & window)
+{
+	bool testFinished = false;
+
+	// Mouse move callback: prints current position
+	window.onMouseMove = [](int x, int y, EzMouseStateBits stateFlags)
+	{
+		printf("Mouse pos: { %d, %d }, %s ¡ª press 'Esc' to finish.\n", x, y, ToString(stateFlags).c_str());
+
+		return 0;
+	};
+
+	// Mouse click callback: prints button and action info
+	window.onMouseClick = [](EzMouseButton button, EzMouseAction action, EzMouseStateBits stateFlags)
+	{
+		printf("%s, %s, %s.\n", ToString(button), ToString(action), ToString(stateFlags).c_str());
+
+		return 0;
+	};
+
+	// Mouse wheel scroll callback ¡ª prints scroll delta
+	window.onWheelScroll = [](int dx, int dy, EzMouseStateBits stateFlags)
+	{
+		if (dx != 0)
+			printf("Mouse wheel scrolled horizontally: %d, %s.\n", dx, ToString(stateFlags).c_str());
+		if (dy != 0)
+			printf("Mouse wheel scrolled vertically: %d, %s.\n", dy, ToString(stateFlags).c_str());
+		return 0;
+	};
+
+	// Keyboard callback: waits for Escape key to end the test
+	window.onKeyboardPress = [&](EzKey key, EzKeyAction action)
+	{
+		if (key == EzKey::Escape && action == EzKeyAction::Press)
+		{
+			testFinished = true;
+		}
+		return 0; // handled
+	};
+
+	printf("Mouse event test started.\n");
+
+	// Main event loop
+	while (window.IsOpen() && !testFinished)
+	{
+		window.WaitProcessEvent();
+	}
+
+	printf("Mouse event test finished.\n\n");
+
+	// Cleanup callbacks
+	window.onKeyboardPress = nullptr;
+	window.onWheelScroll = nullptr;
+	window.onMouseClick = nullptr;
+	window.onMouseMove = nullptr;
+}
